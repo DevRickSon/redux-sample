4d3b19a343d7ae343bbc065a6e003567dc6b795a
jhson <jhson@encar.com>
2017-08-25 18:30:01 +0900

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Button.css';
+
+const Button = ({onCreate, onRemove}) => {
+    return (
+        <div className="Buttons">
+            <div className="btn add" onClick={onCreate}>생성</div>
+            <div className="btn remove" onClick={onRemove}>제거</div>
+        </div>
+    );
+};
+
+Button.propTypes = {
+    onCreate: PropTypes.func,
+    onRemove: PropTypes.func,
+};
+
+Button.defaultProps = {
+    onCreate: () => console.warn('onCreate no defined'),
+    onRemove: () => console.warn('onRemove no defined')
+};
+
+export default Button;
---
+++ src/containers/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import * as actions from '../actions';
+import Button from '../components/Button';
+import CounterListContainer from '../containers/CounterListContainer';
+import {connect} from 'react-redux';
+
+import {getRandomColor} from '../utils';
+
+class App extends Component {
+    render(){
+        const {onCreate, onRemove} = this.props;
+
+        return (
+            <div>
+                <Button
+                    onCreate={onCreate}
+                    onRemove={onRemove}
+                />
+                <CounterListContainer />
+            </div>
+        );
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    onCreate: () => dispatch(actions.create(getRandomColor())),
+    onRemove: () => dispatch(actions.remove())
+});
+
+export default connect(null, mapDispatchToProps)(App);
